f5738860b4de9e37cd14d02d61dacbb765b70182
Damar452 <damarh452@gmail.com>
2021-07-12 11:55:53 -0500

    terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    GifsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
